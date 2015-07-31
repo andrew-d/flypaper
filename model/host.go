09d71f9769e6444347205b83796022ae6e4b0a41
Andrew Dunham <andrew@du.nham.ca>
2015-07-30 23:12:11 -0700

    Make the Host model properly handle NULL columns

--- model/host.go
+++ model/host.go
@@ -1,9 +1,14 @@
 package model
 
+import (
+	"database/sql"
+)
+
 type Host struct {
-	ID        int64  `db:"id"        json:"id"`
-	IpAddress string `db:"ipaddress" json:"ipaddress"`
-	Hostname  string `db:"hostname"  json:"hostname"`
+	ID        int64          `db:"id"        json:"id"`
+	IpAddress string         `db:"ipaddress" json:"ipaddress"`
+	Hostname  sql.NullString `db:"hostname"  json:"hostname"`
+	Region    sql.NullInt64  `db:"region"    json:"region"`
 
 	// TODO: marking a host as "standby", so it's still saved but not shown,
 	// scanned by default, etc.?
