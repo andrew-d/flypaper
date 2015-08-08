276faf60355cfc36a966e17715db8e6b3376e793
Andrew Dunham <andrew@du.nham.ca>
2015-08-07 19:41:54 -0700

    Show 'none' when there's no start or end time for a region

--- frontend/app/pages/Regions.jsx
+++ frontend/app/pages/Regions.jsx
@@ -38,11 +38,11 @@ class Regions extends React.Component {
   renderRegions() {
     return map(this.props.regions, (region) => {
       const test_start = region.test_start ?
-        moment(region.test_start).format("dddd, MMMM Do YYYY, h:mm:ss a") :
-        '';
+        moment(region.test_start).format('dddd, MMMM Do YYYY, h:mm:ss a') :
+        <i>none</i>;
       const test_end = region.test_end ?
-        moment(region.test_end).format("dddd, MMMM Do YYYY, h:mm:ss a") :
-        '';
+        moment(region.test_end).format('dddd, MMMM Do YYYY, h:mm:ss a') :
+        <i>none</i>;
 
       return (
         <TagLink
